1ffa0b06d49b9639665af75522550287069c9832
Igor Leonenko <igrleon@gmail.com>
2025-03-21 12:13:45 +0100

    routing and views improvements

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module PowerYourMeal
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     config.autoload_paths << Rails.root.join("app", "services")
+    config.autoload_paths << Rails.root.join("app", "lib")
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq"
+
   resources :meals do
     post :get_random_meal, on: :collection
   end
 
-  resource :registration, only: %i[new create]
-  resource :session, only: %i[new create destroy]
+  resource  :registration, only: %i[new create]
+  resource  :session, only: %i[new create destroy]
   resources :passwords, only: %i[new create edit update], param: :token
 
   get "up" => "rails/health#show", as: :rails_health_check
