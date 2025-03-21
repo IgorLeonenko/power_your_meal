bd43cd80bef655c6a9776247798dc50defcdeed3
Igor Leonenko <igrleon@gmail.com>
2025-03-21 12:13:21 +0100

    meals controller base setup

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,13 +1,17 @@
 class MealsController < ApplicationController
-  def index;end
+  def index
+    @meals = Meal.all
+  end
 
   def show
     @meal = Meal.find(params[:id])
   end
 
   def get_random_meal
-    meal = ThemealdbClient.new.get_random_meal
+    meal = Themealdb.new.get_random_meal
+    return redirect_to meal_path(meal) if meal
 
-    redirect_to meal_path(meal)
+    flash[:alert] = "Failed to fetch random meal. Please try again."
+    redirect_to meals_path
   end
 end
